15b34ce2df9e117facc73a04ed475888f29b2141
Rafael Moraes de Souza <rafael.souza@programmers.com.br>
2022-05-16 12:06:33 -0300

    Add project files.

--- EX9/EX9.cs
+++ EX9/EX9.cs
@@ -12,11 +12,15 @@ Console.WriteLine("-------------------------------------------------------------
 
 Console.WriteLine("Digite os dois valores: ");
 
-value1 = Convert.ToDouble(Console.ReadLine());
-value2 = Convert.ToDouble(Console.ReadLine());
+value1 = Convert.ToInt32(Console.ReadLine());
+value2 = Convert.ToInt32(Console.ReadLine());
 
 divisao = value1 / value2;
 modularizacao = value2 % value1;
 
-Console.WriteLine(divisao);
-Console.WriteLine(modularizacao);
+
+if (value2 == 0)
+    Console.WriteLine("Você não pode dividir por 0");
+else
+    Console.WriteLine(divisao);
+    Console.WriteLine(modularizacao);
